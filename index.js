447615220b53850f2b4f05dca932da71c2e9c89a
Santi Arroyo <santi.arroyo551@gmail.com>
2022-07-05 09:48:22 -0400

    commit tree fa04d9c8b5067a7db05a6b64740f7777e84eb15d

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+   return fetch('https://anapioficeandfire.com/api/books')
+    .then((response) => response.json())
+    .then(data => renderBooks(data))
+  }
+ 
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
+    console.log(book.name)
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
